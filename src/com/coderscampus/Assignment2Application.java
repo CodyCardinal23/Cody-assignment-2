9a6ce7881a2270368358a649c1d317ed5fc2d443
CodyCardinal23 <codycardinal@gmail.com>
2024-01-14 14:27:21 -0700

    Final draft

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,10 +1,54 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Random random = new Random();
+		int theRandomNumber = random.nextInt(100) + 1;
+		
+		int numberOfGuesses = 0;
+		int maxGuesses = 5;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while (numberOfGuesses < maxGuesses) {
+			System.out.print("Pick a number between 1  and 100: ");
+			
+			if (scanner.hasNextInt()) {
+				int userGuess = scanner.nextInt();
+				
+				if (userGuess >= 1 && userGuess <= 100) {
+					numberOfGuesses++;
+					
+					if (userGuess == theRandomNumber) {
+						System.out.println("You win!\nThe number to guess was: " + theRandomNumber);
+						break;
+						
+					}else {
+						System.out.println(userGuess < theRandomNumber ? "Please pick a higher number" : "Please pick a lower number");
+					
+					}
+				}else{
+					System.out.println("Your guess is not between 1 and 100, please try again");
+				}
+			}else {
+				System.out.println("Invalid input. Please enter a valid integer.");
+				scanner.next();
+			}
+			
+			scanner.nextLine();
+		}
+						if (numberOfGuesses == maxGuesses) {
+                            System.out.println("You lose!\nThe number to guess was: " + theRandomNumber);
+                        }//To match what the intructions said, I googled how to make text go on the line underneath instead of right next to it
+						//I thought this would be easier than to write more code. "\n"
+						
+						scanner.close();
 
 	}
-
 }
